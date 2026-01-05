0acff7a73670ebca2ea21e321a9f1246ce24c6ed
cz3ch0 <jerichocstro@gmail.com>
2026-01-05 17:51:13 +0800

    add activity - arithmeticOperators

---
+++ arithmeticOperators.py
@@ -0,0 +1,18 @@
+# aritmetic operators 
+friends = 5
+
+#friends = friends + 1
+#friends += 1
+
+#friends = friends - 1
+#friends -= 1
+
+#friends = friends * 2
+#friends *= 2
+
+#friends = friends / 2 
+#friends /= 2
+
+friends = friends ** 2
+friends **= 2
+print(friends)

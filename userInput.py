f2f50114f0ba675ff9eef85c50e68881d87446be
cz3ch0 <jerichocstro@gmail.com>
2026-01-07 17:16:59 +0800

    topic for user input, and use of len(), upper() and lower()

---
+++ userInput.py
@@ -0,0 +1,6 @@
+# User input using strings: - accepting external data from the user.
+# legth = # of char; len(word) ; len()
+# upper() , lower()
+
+word = input('enter a word:')
+print('i am' + ' ' + word)

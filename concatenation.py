30fd4e9f2d836f513c6161a21cae5f51fa331e99
cz3ch0 <jerichocstro@gmail.com>
2026-01-07 17:07:50 +0800

    update to the code

---
+++ concatenation.py
@@ -0,0 +1,13 @@
+# concatenation: Joining/Combining two things. Strings
+
+# 1. + 
+# 2. using join() method
+# 3. string formatting
+# 4. % old string formatting
+
+name = "Boss"
+greetings = "hello %s" % name
+
+print(greetings)
+
+
